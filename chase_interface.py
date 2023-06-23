7e7027af7e26e648056b268f12c9b60a9fd3c4df
goldenxuett <ttanpcs@gmail.com>
2023-06-23 10:59:08 -0500

    Add reset and default functionality

--- chase_interface.py
+++ chase_interface.py
@@ -11,8 +11,10 @@ credit_path = basedir / 'credit_statements'
 debit_path = basedir / 'debit_statements'
 mapping_path = basedir / 'mapping.json'
 
-def create_session():
+def create_session(reset):
     engine = create_engine(f"sqlite:///{os.path.join(basedir, 'finances.sqlite')}")
+    if reset:
+        Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind = engine)
     session = Session()
@@ -24,13 +26,12 @@ def generate():
     parser.add_argument("--credit", "-c", type=Path, default=credit_path, help="Credit card statement path")
     parser.add_argument("--debit", "-d", type=Path, default=debit_path, help="Debit card statement path")
     parser.add_argument("--mapping", "-m", type=Path, default=mapping_path, help="Regexes used to map categories")
-    # TODO: Make option to reset database, and make option for default rather than mandated user input
-
+    parser.add_argument("--default", type=str, default=None, help="Default category")
+    parser.add_argument("--reset", "-r", action='store_true', help="Apply flag to reset database")
     args = parser.parse_args()
-    session = create_session()
-    parse_files(args.mapping, args.credit, args.debit, session)
+    session = create_session(args.reset)
+    parse_files(args.mapping, args.credit, args.debit, args.default, session)
 
-    # Create a file that does the stuff below
     # Summary statistics using plotly
     # then create summary statistics? / csvs from this information
 
--- parsing.py
+++ parsing.py
@@ -20,30 +20,32 @@ def find_new_files(directory, type, session):
             new_files.append(path)
     return new_files
 
-def determine_type(transaction, regex_map):
+def determine_type(transaction, default, regex_map):
     for label in regex_map:
         if re.search("|".join(regex_map[label]), transaction) is not None:
             return label
-        
-    system('cls')
-    print({i: list(regex_map.keys())[i] for i in range(len(regex_map))})
-    print(transaction)
-    label_index = input()
-    while not (label_index.isdigit() and int(label_index) < len(regex_map) and int(label_index) > -1):
+    if default is None:
+        system('cls')
+        print({i: list(regex_map.keys())[i] for i in range(len(regex_map))})
+        print(transaction)
         label_index = input()
-    label_index = int(label_index)
-    print("Add label? (y/n)")
-    choice = input()
-    if choice == 'y':
-        print("Type new search term:")
-        new_term = input()
-        regex_map[list(regex_map.keys())[label_index]].append(new_term)
+        while not (label_index.isdigit() and int(label_index) < len(regex_map) and int(label_index) > -1):
+            label_index = input()
+        label_index = int(label_index)
+        print("Add label? (y/n)")
+        choice = input()
+        if choice == 'y':
+            print("Type new search term:")
+            new_term = input()
+            regex_map[list(regex_map.keys())[label_index]].append(new_term)
 
-    return list(regex_map.keys())[label_index]
+        return list(regex_map.keys())[label_index]
+    else:
+        return default
 
-def read_files(files, session, regex_map, dropped_columns, renamed_map, transaction_type):
+def read_files(files, default, session, regex_map, dropped_columns, renamed_map, transaction_type):
     def re_type(transaction):
-        return determine_type(transaction, regex_map)
+        return determine_type(transaction, default, regex_map)
     
     for f in files:
         df = pd.read_csv(str(f), index_col=False)
@@ -55,10 +57,11 @@ def read_files(files, session, regex_map, dropped_columns, renamed_map, transact
         df.to_sql('transactions', session.bind, if_exists="append", index=False)
     session.commit()
 
-def read_credit_files(credit_file_path, session, regex_map):
+def read_credit_files(credit_file_path, default, session, regex_map):
     credit_files = find_new_files(credit_file_path, "credit", session)
     read_files(
         credit_files,
+        default,
         session,
         regex_map,
         ['Transaction Date', 'Category', 'Type', 'Memo'],
@@ -66,10 +69,11 @@ def read_credit_files(credit_file_path, session, regex_map):
         "credit"
     )
 
-def read_debit_files(debit_file_path, session, regex_map):
+def read_debit_files(debit_file_path, default, session, regex_map):
     debit_files = find_new_files(debit_file_path, "debit", session)
     read_files(
         debit_files,
+        default,
         session,
         regex_map,
         ['Details', 'Type', 'Balance', 'Check or Slip #'],
@@ -77,8 +81,8 @@ def read_debit_files(debit_file_path, session, regex_map):
         "debit"
     )
 
-def parse_files(mapping, credit_file_path, debit_file_path, session):
+def parse_files(mapping, credit_file_path, debit_file_path, default, session):
     regex_map = read_from_mapping(mapping)
-    read_credit_files(credit_file_path, session, regex_map)
-    read_debit_files(debit_file_path, session, regex_map)
+    read_credit_files(credit_file_path, default, session, regex_map)
+    read_debit_files(debit_file_path, default, session, regex_map)
     write_to_mapping(mapping, regex_map)
