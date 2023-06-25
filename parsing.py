212df1dcd6a545095e2d50ae3106ee23c6a64ca2
goldenxuett <ttanpcs@gmail.com>
2023-06-24 21:02:11 -0500

    Implement multiple statement duplication check and datetime conversion

--- parsing.py
+++ parsing.py
@@ -1,8 +1,9 @@
 from os import system
 from json import load, dump
 import re
-from models import Statement
+from models import Statement, Transaction
 import pandas as pd
+from datetime import datetime
 
 def read_from_mapping(regex_map_path):
     with open(regex_map_path, 'r') as f:
@@ -20,14 +21,21 @@ def find_new_files(directory, type, session):
             new_files.append(path)
     return new_files
 
-def determine_type(transaction, default, regex_map):
+def determine_type(description, date, session, default, regex_map):
+    # Avoid duplicate transactions across different files
+    if session.query(Transaction.id).filter(Transaction.description == description, Transaction.date == date.to_pydatetime()).all() != []:
+        return "Drop"
+    
+    # Check for regex rules
     for label in regex_map:
-        if re.search("|".join(regex_map[label]), transaction) is not None:
+        if re.search("|".join(regex_map[label]), description) is not None:
             return label
+
+    # Manual user input if default is not set
     if default is None:
         system('cls')
         print({i: list(regex_map.keys())[i] for i in range(len(regex_map))})
-        print(transaction)
+        print(description)
         label_index = input()
         while not (label_index.isdigit() and int(label_index) < len(regex_map) and int(label_index) > -1):
             label_index = input()
@@ -44,18 +52,25 @@ def determine_type(transaction, default, regex_map):
         return default
 
 def read_files(files, default, session, regex_map, dropped_columns, renamed_map, transaction_type):
-    def re_type(transaction):
-        return determine_type(transaction, default, regex_map)
-    
+    format = '%m/%d/%Y'
+    def re_type(description, date):
+        return determine_type(description, date, session, default, regex_map)
+
+    def re_date(unformatted_date):
+        return datetime.strptime(unformatted_date, format)
+
     for f in files:
         df = pd.read_csv(str(f), index_col=False)
         df.drop(columns=dropped_columns, inplace=True)
         df.rename(columns=renamed_map, inplace=True)
-        df['Type'] = df['Description'].map(re_type)
+        df['Date'] = df['Date'].map(re_date)
+        df['Type'] = list(map(re_type, df['Description'], df['Date']))
+
+        df.drop(df[(df['Type'] == 'Drop')].index , inplace=True)
         current_statement = Statement(name = f.name, type = transaction_type)
         session.add(current_statement)
-        df.to_sql('transactions', session.bind, if_exists="append", index=False)
-    session.commit()
+        df.to_sql('transactions', session.bind, if_exists="append", index=False) # Need to do something instead of to_sql that checks for if records already exist
+        session.commit()
 
 def read_credit_files(credit_file_path, default, session, regex_map):
     credit_files = find_new_files(credit_file_path, "credit", session)
